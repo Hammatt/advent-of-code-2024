217d23a3b574cf4ed84d508a05794746dbea930f
Alexander Hammatt <alexander.hammatt@protonmail.com>
2024-12-06 22:19:40 +1300

    I spend ages doing this trying ot write fancy parsers and lexers and all of that jazz
    I mostly learnt that if that is the problem I have that golang probably is not the best tool for the job (I miss rust's nom)
    
    Regex FTW

--- day3/day3.go
+++ day3/day3.go
@@ -16,18 +16,29 @@ func main() {
 
 	input := string(b)
 
-	regex := regexp.MustCompile("mul\\([0-9]+,[0-9]+\\)")
+	regex := regexp.MustCompile("(mul\\([0-9]+,[0-9]+\\))|(do\\(\\))|(don't\\(\\))")
 	matches := regex.FindAllString(input, -1)
 
 	answer := 0
 
+	on := true
 	for _, match := range matches {
-		argsStr := match[4 : len(match)-1]
-		args := strings.Split(argsStr, ",")
-		l, _ := strconv.Atoi(args[0])
-		r, _ := strconv.Atoi(args[1])
+		if match[:3] == "mul" {
+			if on {
+				argsStr := match[4 : len(match)-1]
+				args := strings.Split(argsStr, ",")
+				l, _ := strconv.Atoi(args[0])
+				r, _ := strconv.Atoi(args[1])
 
-		answer += l * r
+				answer += l * r
+			}
+		} else if match == "do()" {
+			on = true
+		} else if match == "don't()" {
+			on = false
+		} else {
+			panic(match)
+		}
 	}
 
 	fmt.Println(answer)
