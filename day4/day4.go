63238db0e7018b310e032864d3fce81c891629f5
Alexander Hammatt <alexander.hammatt@protonmail.com>
2024-12-08 22:57:36 +1300

    day4 part 2

--- day4/day4.go
+++ day4/day4.go
@@ -5,14 +5,8 @@ import (
 	"bytes"
 	"fmt"
 	"os"
+	"strings"
 )
-
-type direction struct {
-	y int
-	x int
-}
-
-var tileSearchDirections = [...]direction{{-1, 0}, {0, -1}, {1, 0}, {0, 1}, {-1, -1}, {1, 1}, {-1, 1}, {1, -1}}
 
 func main() {
 	b, err := os.ReadFile("input.txt")
@@ -20,16 +14,21 @@ func main() {
 		panic(err)
 	}
 
-	answerRunes := []rune("XMAS")
-
 	grid := loadGrid(b)
 	count := 0
 
-	for i := 0; i < len(grid); i++ {
+	for i := 1; i < len(grid)-1; i++ {
 		row := grid[i]
-		for j := 0; j < len(row); j++ {
-			for _, tsd := range tileSearchDirections {
-				if tryTile(grid, i, j, answerRunes, 0, tsd) {
+		for j := 1; j < len(row)-1; j++ {
+			r := grid[i][j]
+			if r == 'A' {
+				var b strings.Builder
+				b.WriteRune(grid[i-1][j-1])
+				b.WriteRune(grid[i-1][j+1])
+				b.WriteRune(grid[i+1][j+1])
+				b.WriteRune(grid[i+1][j-1])
+
+				if b.String() == "MMSS" || b.String() == "MSSM" || b.String() == "SSMM" || b.String() == "SMMS" {
 					count++
 				}
 			}
@@ -50,24 +49,4 @@ func loadGrid(b []byte) [][]rune {
 	}
 
 	return grid
-}
-
-func tryTile(grid [][]rune, i, j int, word []rune, wordIndex int, tsd direction) bool {
-	t := grid[i][j]
-	w := word[wordIndex]
-
-	if t != w {
-		return false
-	} else if t == w && wordIndex == len(word)-1 {
-		return true
-	}
-
-	ni := i + tsd.y
-	nj := j + tsd.x
-
-	if ni >= 0 && ni < len(grid) && nj >= 0 && nj < len(grid[ni]) && tryTile(grid, ni, nj, word, wordIndex+1, tsd) {
-		return true
-	}
-
-	return false
 }
